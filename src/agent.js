86335e8d28bf4778e5ea23b203a4a0f3495c4fe1
xueting77 <xueting.wang77@gmail.com>
2023-01-29 13:27:29 +1100

    222

--- src/agent.js
+++ src/agent.js
@@ -1,4 +1,5 @@
 import axios from "axios";
 
+
 // export const instance = axios.create({baseURL: "http://localhost:8000"});
 export const instance = axios.create({baseURL: "https://tyra-blog-backend.herokuapp.com"});
